f3fae8e409a0c36d6d04e0274f3b6556f1d819cb
Jallen Hunnid <aubinderulo@gmail.com>
2026-05-14 22:13:28 +0200

    Navbar elements Styled

--- netflix-lan/src/components/Home.jsx
+++ netflix-lan/src/components/Home.jsx
@@ -2,9 +2,10 @@ import {Languages} from 'lucide-react'
 function Home () {
     return (
         <nav className='flex justify-between px-25 py-5' >
-            <h1 className="text-7xl text-red-600">NETFLIX</h1>
+            <h1 className="text-7xl  text-red-500">NETFLIX</h1>
             <ul className="flex justify-items-center text-white">
-                <li className='flex justify-items-center'>
+                <div className=' text-md font-medium flex justify-center border border-white/30 rounded-md w-36 h-10 py-2 mx-3'>
+                    <li>
                     <Languages/>
                     </li>
                     <li>
@@ -13,8 +14,9 @@ function Home () {
                         <option value="">Francais</option>
                     </select>
                 </li>
+                </div>
                 <li>
-                    <button type="submit">Sign in</button>
+                    <button type="submit" className='bg-red-600 w-24 h-10 px-3 rounded-md font-medium'>Sign in</button>
                 </li>
             </ul>
         </nav>
