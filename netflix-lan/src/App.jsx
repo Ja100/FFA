9dc01451a787b364d2340a15735d2eed73ed2da7
Jallen Hunnid <aubinderulo@gmail.com>
2026-05-17 20:36:01 +0200

    Styled input placeholder

--- netflix-lan/src/App.jsx
+++ netflix-lan/src/App.jsx
@@ -7,13 +7,14 @@ function App() {
     <>
      <Home/>
 
-     <div className="text-white w-3xl text-center flex items-center justify-center mx-120 my-40  h-60 flex-col gap-5">
+     <div className="text-white w-3xl text-center flex items-center justify-center mx-120 my-60 h-60 flex-col gap-5">
         <h1 className="text-7xl font-bold">Unlimited movies, TV shows, and more</h1>
      <h3 className="text-lg font-bold">Starts at US$2.99. Cancel anytime.</h3>
      <p className="text-lg font-medium">Ready to watch? Enter your email to create or restart your membership.</p>
+     
      <div className="flex gap-2">
-        <input type="text" placeholder="Email Address" className='text-md font-medium flex justify-center border border-white/30 rounded-md w-full h-14 px-4'/>
-        <button className='flex justify-center items-center bg-red-600 w-xs text-2xl h-14  rounded-md font-medium'>Get Started
+        <input type="text" placeholder="Email Address" className='bg-zinc-900/70 text-xl font-light placeholder:text-zinc-300 placeholder:font-normal placeholder:font-sans flex justify-center border border-white/30 rounded-md w-full h-14 px-4'/>
+        <button className='flex justify-center items-center gap-4 bg-red-600 hover:bg-red-700 w-xs text-2xl h-14 rounded-md font-semibold'>Get Started
           <ChevronRight/>
         </button>
      </div>
