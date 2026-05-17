893eadd8acdb63891619fc0b5a2e27d54744e802
Jallen Hunnid <aubinderulo@gmail.com>
2026-05-17 16:49:37 +0200

    Input & button styled

--- netflix-lan/src/App.jsx
+++ netflix-lan/src/App.jsx
@@ -1,5 +1,5 @@
 import Home from "./components/Home.jsx"
-
+import { ChevronRight } from "lucide-react"
 
 function App() {
 
@@ -7,12 +7,16 @@ function App() {
     <>
      <Home/>
 
-     <div className="text-white w-3xl flex items-center justify-center mx-100 my-65  h-60 flex-col gap-5">
-        <h1 className="text-7xl text-center font-bold">Unlimited movies, TV shows, and more</h1>
-     <h3>Starts at US$2.99. Cancel anytime.</h3>
-     <p>Ready to watch? Enter your email to create or restart your membership.</p>
-     <input type="text" placeholder="Email Address" className='text-md font-medium flex justify-center border border-white/30 rounded-md w-36 h-10 py-2 mx-3'/>
-     <button className='bg-red-600 w-32 h-10 px-3 rounded-md font-medium'>Get Started </button>
+     <div className="text-white w-3xl text-center flex items-center justify-center mx-120 my-40  h-60 flex-col gap-5">
+        <h1 className="text-7xl font-bold">Unlimited movies, TV shows, and more</h1>
+     <h3 className="text-lg font-bold">Starts at US$2.99. Cancel anytime.</h3>
+     <p className="text-lg font-medium">Ready to watch? Enter your email to create or restart your membership.</p>
+     <div className="flex gap-2">
+        <input type="text" placeholder="Email Address" className='text-md font-medium flex justify-center border border-white/30 rounded-md w-full h-14 px-4'/>
+        <button className='flex justify-center items-center bg-red-600 w-xs text-2xl h-14  rounded-md font-medium'>Get Started
+          <ChevronRight/>
+        </button>
+     </div>
      </div>
      
     </>
