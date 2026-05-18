5489a50140b39268387abb1a335df7d37e7bd1d1
Jallen Hunnid <aubinderulo@gmail.com>
2026-05-18 12:27:10 +0200

    Form Validation Added

--- react-project/src/pages/login.jsx
+++ react-project/src/pages/login.jsx
@@ -1,12 +1,31 @@
 import { useState } from "react"
 function Login(){
     const [username, setUsername] = useState("");
-    const [passwword, setPassword] = useState("");
+    const [password, setPassword] = useState("");
+    function handleSubmit(e){
+        e.preventDefault();
+    }
+    function validation() {
+        if(username.length < 5 || !username.includes('@')) {
+            console.log('username must be 5 characters long containing @'); 
+            return;  
+        }
+        const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d).+$/;
+        if(!passwordRegex.test(password) || password.length < 8){
+            console.log('password must be 8 characters long containing both numbers and letters');
+            return;
+            
+        }  
+         
+        console.log('Validation Passed..!');
+        
+    }
+
 
     return(
     <>
     <div className="flex justify-center">
-        <form action="" 
+        <form action="" onClick={handleSubmit} 
             className=" flex flex-col gap-4 justify-center items-center my-50 w-100 border bg-blue-300 text-xl py-5 px-3">
             <h1>Login Form</h1>
             <input type="text" placeholder="Username"
@@ -16,13 +35,14 @@ function Login(){
             className="border-2 rounded-md px-3 h-10 placeholder:text-white"/>
             <input type="password" placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
-            value={passwword}
+            value={password}
             className="border-2 rounded-md px-3 h-10  placeholder:text-white"/>
             <button
+            onClick={validation}
             className="border-2 rounded-md font-semibold w-30 bg-blue-50 h-10 cursor-pointer hover:bg-blue-600">
                 Log in</button>
             <p>{username}</p>
-            <p>{passwword}</p>
+            <p>{password}</p>
         </form>
     </div>
     </>
