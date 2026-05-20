07b202d9b7775f9d33434a656cb053a06ceb62c4
Jallen Hunnid <aubinderulo@gmail.com>
2026-05-20 10:46:58 +0200

    Navigate import added

--- react-project/src/pages/dashboard.jsx
+++ react-project/src/pages/dashboard.jsx
@@ -1,9 +1,9 @@
 function Dashboard() {
     return(
     <>
-    <div className="">
-        <h1>Hey Jallen</h1>
-        <p>Welcome to DASHBOARD</p>
+    <div className="bg-blue-300 text-xl flex gap-2">
+        <h1>Hello, <span className=" text-gray-100 font-medium">Chris!</span></h1>
+        <p className="">Dashboard</p>
     </div>
     </>)
 } 
